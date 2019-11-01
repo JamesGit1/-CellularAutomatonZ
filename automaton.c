865ec1e3f7ffb1f1b15e57167a360effcad64e1d
JamesGit1 <43144127+JamesGit1@users.noreply.github.com>
2019-11-01 16:02:34 +0000

    Update automaton.c

--- automaton.c
+++ automaton.c
@@ -6,68 +6,14 @@
 int displayMenu()
 {
     printf("What would you like to select\n\n");
-    printf("Option 1. ----- \n");
-    printf("Option 2. ----- \n");
-    printf("Option 3. ----- \n");
-    printf("4. Add more options\n");
+    printf("Option 1. Rule 30 Automaton \n");
+    printf("Option 2. Rule 60 Automaton \n");
+    printf("Option 3. Custom Rule Automaton \n");
     printf("0. Exit\n");
 }
-
-int rules()
-{
-    int option = -1;
-
-    int generations;
-    // TODO add extra rules
-    int rule30[8] = {0,0,0,1,1,1,1,0};
-    int rule60[8] = {0,0,1,1,1,1,0,0};
-    int ruleUser[8];
-// TODO Input Validation
-    do
-    {
-        displayMenu();
-        scanf("%d", &option);
-
-
-        switch (option)
-        {
-        case 1:
-            printf("You have selected rule 30\n");
-            //TODO Input validation
-            printf("How many generations would you like to run\n");
-            scanf("%d", &generations);
-            runAutomaton(generations,rule30);
-            break;
-        case 2:
-            printf("You have selected rule 60\n");
-            //TODO Input validation
-            printf("How many generations would you like to run\n");
-            scanf("%d", &generations);
-            runAutomaton(generations,rule60);
-            break;
-        case 3:
-            printf("You have selected custom rule\n");
-
-            // TODO ask for each bit while loop won't increemnt until valid number or he presse 9 exit
-            // TODO ask for gen between 2 and 256
-            // TODO also checks what rule it is by converting the binary vaule of the array by using Andrews thing and by concat the user input into 2 variables
-            printf("How many generations would you like to run\n");
-            scanf("%d", &generations);
-            runAutomaton(generations,ruleUser);
-            break;
-        case 0:
-            printf("Goodbye");
-            break;
-        default:
-            printf("Please type in a valid number\n");
-            break;
-        }
-    }
-    while(option!=0);
-}
-
 // checks all neighbouring cells
-int checks(int p[], int c[],int current)
+// TODO add generation size to be custom
+int checks(int p[], int c[],int current, int size)
 {
 // checks all neighbouring cells
 // outputs it to child
@@ -78,13 +24,13 @@ int checks(int p[], int c[],int current)
 // Need to pass in size so i can cycle     
     if(current == 0)
     {
-        left = 7;
+        left = size--;
         right = 1;
     }
-    else if(current == 7)
+    else if(current == size--)
     {
-        left = 6;
-        right = 1;
+        left = current--;
+        right = 0;
     }
     else
     {
@@ -130,22 +76,38 @@ int checks(int p[], int c[],int current)
 
 
 }
-int runAutomaton(int gen,int rule[])
+
+int firstGen(int *parentp, int size){
+    printf("Please define \n");
+    for (int i=0; i<size; ++i){
+        int answer=-1; 
+        while(!(answer==1 || answer==0)){
+            printf("Please enter 1 or 0:\n[%d]: ",i);
+            scanf("%d", &answer);
+        }
+
+        parentp[i] = answer;  
+    }
+
+}
+
+int runAutomaton(int gen,int rule[],int size)
 {
     // TODO Make it so the One is at a random spot or let user pick
     // TODO ask for user how big the array is
-    int	parent[8] = {0,0,0,0,1,0,0,0};
-    int	child[8] = {0};
+    int parent[size];
+
+    int child[size];
     int state;
     int counter = 0;
 
     do
     {
-    	// Size of parent
-        for(int i = 0 ; i <=7; i++)
+        // Size of parent
+        for(int i = 0 ; i < size; i++)
         {
-        		// Write binary in here
-            state = checks(parent,child,i);
+                // Write binary in here
+            state = checks(parent,child,i,size);
             child[i] = rule[state];
             printf("%d",parent[i]);
 
@@ -165,6 +127,84 @@ int runAutomaton(int gen,int rule[])
     }
     while(gen != counter);
 
+}
+
+int rules()
+{
+    int option = -1;
+
+    int generations;
+    // TODO add extra rules
+    int rule30[8] = {0,0,0,1,1,1,1,0};
+    int rule60[8] = {0,0,1,1,1,1,0,0};
+    int ruleUser[8];
+    int size;
+// TODO Input Validation
+    do
+    {
+        displayMenu();
+        scanf("%d", &option);
+
+
+        switch (option)
+        {
+        case 1:
+            printf("You have selected rule 30\n");
+            printf("Please enter the size of each generation:");
+            scanf("%d", &size);
+            //TODO Input validation
+            printf("How many generations would you like to run: ");
+            scanf("%d", &generations);
+            runAutomaton(generations,rule30,size);
+            break;
+        case 2:
+            printf("You have selected rule 60\n");
+            printf("Please enter the size of each generation:");
+            scanf("%d", &size);
+            //TODO Input validation
+            printf("How many generations would you like to run: ");
+            scanf("%d", &generations);
+            runAutomaton(generations,rule60,size);
+            break;
+        case 3:
+            printf("You have selected custom rule\n");
+            printf("Please enter the size of each generation:");
+            scanf("%d", &size);
+
+            // TODO ask for each bit while loop won't increemnt until valid number or he presse 9 exit
+            // TODO ask for gen between 2 and 256
+            // TODO also checks what rule it is by converting the binary vaule of the array by using Andrews thing and by concat the user input into 2 variables
+            printf("How many generations would you like to run\n");
+            scanf("%d", &generations);
+
+            printf("Please enter the custom rule 1 or 0s \n");
+            printf("e.g. 010101101 \n\n");
+            for (int i=0; i<8; ++i)
+            {
+                int answer=-1; 
+                while(!(answer==1 || answer==0)){
+                    printf("Please enter 1 or 0:\n[%d]: ",i);
+                    scanf("%d", &answer);
+                }
+
+                ruleUser[i] = answer;
+            }
+            printf("Rule Set:\n");
+            for (int i=0; i<8; ++i)
+                printf("%d",ruleUser[i]);
+
+            printf("\n\nAUTOMATON\n");
+            runAutomaton(generations,ruleUser,size);
+            break;
+        case 0:
+            printf("Goodbye");
+            break;
+        default:
+            printf("Please type in a valid number\n");
+            break;
+        }
+    }
+    while(option!=0);
 }
 
 int main()
